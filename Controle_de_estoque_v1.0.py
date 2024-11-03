6cbd475a80aea70847d9e86ba461ef646090b450
eversonferreira <eversonaraujo589@gmail.com>
2024-11-03 20:52:48 -0300

    Primeiro commit

--- Controle_de_estoque_v1.0.py
+++ Controle_de_estoque_v1.0.py
@@ -1,1 +1,6 @@
+lista_produtos = ['máscara faciais' , 'esmaltes' , 'perfumes' , 'batons']
 
+def mostrar_produtos():
+    print("\nLista de produtos: ")
+    for i, produto in enumerate(lista_produtos):
+        print(f"{i + 1}: {produto}")
