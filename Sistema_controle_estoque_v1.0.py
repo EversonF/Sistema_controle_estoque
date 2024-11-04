303f09c5d53645cb4b44f19c32d7d825983134b0
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2024-11-03 21:58:25 -0300

    feat: Adicionando o metodo de excluir_produto

--- Sistema_controle_estoque_v1.0.py
+++ Sistema_controle_estoque_v1.0.py
@@ -15,3 +15,10 @@ def alterar_produto(indice, novo_produto):
         print(f"Produto na posição {indice + 1} alterado para {novo_produto}.")
     else:
         print("Índice inválido.")
+
+def excluir_produto(indice):
+    if 0 <= indice < len(lista_produtos):
+        excluido = lista_produtos.pop(indice)
+        print(f"{excluido} excluído da lista.")
+    else:
+        print("Índice inválido.")
