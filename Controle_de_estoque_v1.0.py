aa1b941366fbc970aba115fd41bfeff7d0b218e8
Flavia Kethellen <flavia.kethellen@hotmail.com>
2024-11-03 21:07:07 -0300

    Segunda função

--- Controle_de_estoque_v1.0.py
+++ Controle_de_estoque_v1.0.py
@@ -4,3 +4,7 @@ def mostrar_produtos():
     print("\nLista de produtos: ")
     for i, produto in enumerate(lista_produtos):
         print(f"{i + 1}: {produto}")
+
+def adicionar_produto(produto):
+    lista_produtos.append(produto)
+    print(f"{produto} adicionado à lista.")
